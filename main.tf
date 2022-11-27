57e0c108f39531441e022e95720d078acb28558f
Tapan <potdartapan@gmail.com>
2022-11-26 19:51:34 -0500

    commenting out current stuff

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-resource "random_pet" "rg_name" {
-  prefix = var.resource_group_name_prefix
-}
+# resource "random_pet" "rg_name" {
+#   prefix = var.resource_group_name_prefix
+# }
 
-resource "azurerm_resource_group" "rg" {
-  location = var.resource_group_location
-  name     = random_pet.rg_name.id
-}
-provider "azurerm" {
-features {}
-}
+# resource "azurerm_resource_group" "rg" {
+#   location = var.resource_group_location
+#   name     = random_pet.rg_name.id
+# }
+# provider "azurerm" {
+# features {}
+# }
