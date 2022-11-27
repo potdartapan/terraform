a0a31dee21a06d481cb2948cb0bbaf2ac44c03ee
Tapan <potdartapan@gmail.com>
2022-11-26 22:42:26 -0500

    commit

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+<<<<<<< HEAD
+=======
+}
+
+output "public_ip_address" {
+  value = azurerm_linux_virtual_machine.my_terraform_vm.public_ip_address
+}
+
+output "tls_private_key" {
+  value     = tls_private_key.example_ssh.private_key_pem
+  sensitive = true
+>>>>>>> f76fcb9 (Terraform code)
+}
